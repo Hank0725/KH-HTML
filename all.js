7c3e09b4b2183398949630563baffc3c3fbe1df1
Hank0725 <hank2929@hotmail.com>
2020-03-02 12:08:40 +0800

    0302update

--- all.js
+++ all.js
@@ -1,3 +1,6 @@
+//宣告各個節點的變數
+var optionlist = document.querySelector(".optionlist");
+var hotzone = document.querySelector(".hotzone");
 var option = document.querySelector(".option")
 var list = document.querySelector(".list");
 var text = document.querySelector(".text");
@@ -16,10 +19,11 @@ xhr.onload = function () {
     let data = JSON.parse(xhr.responseText);
     var length = data.result.records.length;
     var zone = [];
+    //新增區域陣列
     for (let i = 0; i < length; i++) {
         zone.push(data.result.records[i].Zone);
     };
-    //新增不重複陣列
+
     // 再用 foreach 去判斷陣列裡面所有值是否有吻合
     var dataoption = [];
     zone.forEach(function (value) {
@@ -28,10 +32,6 @@ xhr.onload = function () {
         }
     });
 
-    //宣告各個節點的變數
-    var optionlist = document.querySelector(".optionlist");
-    var hotzone = document.querySelector(".hotzone");
-    var datalen = data.result.records.length
     //下拉式選單匯入子元素option
     for (let i = 0; i < dataoption.length; i++) {
         const str = document.createElement('option');
@@ -50,33 +50,35 @@ xhr.onload = function () {
         var newdata = [];
         var area = e.target.value;
         var newstr = "";
+        var datalen = data.result.records.length
         for (let i = 0; i < datalen; i++) {
             if (data.result.records[i].Zone == area) {
                 newdata.push(data.result.records[i].Zone);
-                document.querySelector("#text").textContent = data.result.records[i].Zone + data.result.records[i].Zone.length + "筆";
+                document.querySelector("#text").textContent = data.result.records[i].Zone + newdata.length + "筆";
                 newstr += '<div class="spot"><div class="spotbox clearfix"><img class="spotimg" src="' + data.result.records[i].Picture1 + '" alt="' + data.result.records[i].Name + '"><span class="Name">' + data.result.records[i].Name + '</span><span class="Zone">' + data.result.records[i].Zone + '</span><p class="Opentime"><img class="timeicon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_clock.png?raw=true">' + data.result.records[i].Opentime + '</p><p class="Add"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_pin.png?raw=true">' + data.result.records[i].Add + '</p><p class="Tel"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_phone.png?raw=true">' + data.result.records[i].Tel + '</p><span class="Ticketinfo"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_tag.png?raw=true">' + data.result.records[i].Ticketinfo + '</span></div></div>';
-                list.innerHTML = newstr;
-            }
-        }
-    }
 
+            }
+        } list.innerHTML = newstr;
+    }
     //熱門區域按鈕函式
     function goarea(e) {
         list.innerHTML = "";
         var newdata = [];
         var area = e.target.value;
         var newstr = "";
+        var datalen = data.result.records.length
         for (let i = 0; i < datalen; i++) {
             if (data.result.records[i].Zone == area) {
                 newdata.push(data.result.records[i].Zone);
-                document.querySelector("#text").textContent = data.result.records[i].Zone + data.result.records[i].Zone.length + "筆";
+                document.querySelector("#text").textContent = data.result.records[i].Zone + newdata.length + "筆";
                 newstr += '<div class="spot"><div class="spotbox clearfix"><img class="spotimg" src="' + data.result.records[i].Picture1 + '" alt="' + data.result.records[i].Name + '"><span class="Name">' + data.result.records[i].Name + '</span><span class="Zone">' + data.result.records[i].Zone + '</span><p class="Opentime"><img class="timeicon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_clock.png?raw=true">' + data.result.records[i].Opentime + '</p><p class="Add"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_pin.png?raw=true">' + data.result.records[i].Add + '</p><p class="Tel"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_phone.png?raw=true">' + data.result.records[i].Tel + '</p><span class="Ticketinfo"><img class="icon" src="https://github.com/Hank0725/KH-HTML/blob/master/JavaScript_HomeWork-gh-pages/assets/icons_tag.png?raw=true">' + data.result.records[i].Ticketinfo + '</span></div></div>';
-                list.innerHTML = newstr;
+
             }
-        }
+        } list.innerHTML = newstr;
     }
 }
 
+// 回上層按鈕
 var back = document.querySelector(".back");
 back.addEventListener("click", clicktop, false);
 function clicktop() {
